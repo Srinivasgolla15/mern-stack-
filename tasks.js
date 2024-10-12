fac3e33b3f15b1b9ae2ecfaddb7bb929b1e57efb
Srinivasgolla15 <lsrinivas0768@gmail.com>
2024-10-12 23:07:11 +0530

    added this tasks of day 6

---
+++ tasks.js
@@ -0,0 +1,63 @@
+
+// function logger(prefix){
+//     console.log(prefix+" "+"here it is message")
+// }
+
+
+// logger("Sasi");
+// logger("Somu");
+// logger("sagar");
+
+// const jsonString = '{"name": "John", "age": 30, "city": "New York"}'; 
+// const r= JSON.parse(jsonString);
+// console.log(r.name)
+
+
+// const person = { 
+//     name: "Alice", 
+//     age: 25, 
+//     hobbies: ["reading", "swimming"] 
+//     }; 
+
+// const r = JSON.stringify(person);
+// console.log(r);
+
+// const car = { 
+//     brand: "Toyota", 
+//     model: "Camry", 
+//     year: 2020 
+//     }; 
+// console.log(Object.keys(car));
+
+// const car = { 
+//     brand: "Toyota", 
+//     model: "Camry", 
+//     year: 2020 
+//     }; 
+// // console.log(Object.values(car));
+// console.log(Object.entries(car));
+
+// const entries = [['name', 'Alice'], ['age', 25], ['city', 'London']];
+// console.log(Object.fromEntries(entries));
+
+// const obj1 = { a: 1, b: 2 }; 
+// const obj2 = { b: 3, c: 4 }; 
+// console.log( {...obj1,...obj2});
+
+// const map = new Map();
+// map.set("apple",1);
+// map.set("banana",2);
+// map.set("orange",3);
+// if (map.has("banana")){
+//     console.log(map.get("banana"))
+// };
+// for(let [key,value] of map){
+//     console.log(`${key}:${value}`);
+// }
+
+// const pairs = [['a', 1], ['b', 2], ['c', 3]]; 
+// const map1 = new Map(pairs);
+// console.log(map1);
+
+
+
